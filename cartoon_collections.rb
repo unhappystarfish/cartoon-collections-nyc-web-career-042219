78dd884cf51cc971d076c1d899c3a4b020923de8
Jake L <jakelovitz@gmail.com>
2019-04-04 19:29:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if (cheese_types & array).empty? 
+  foods.any? {|food| cheeses.include?(food) }
  
   end
 end
