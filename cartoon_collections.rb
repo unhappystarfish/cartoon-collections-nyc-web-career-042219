aff0049bc48ac9e6be1298f003b71c432df735cc
Jake L <jakelovitz@gmail.com>
2019-04-04 19:26:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,8 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(array)
-  if 
-  
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese|
+    cheese == "chedder" || "gouda" || "camembert"
+  end
 end
